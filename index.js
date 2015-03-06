72bfb11f5922d2b2eebf03d297f84e2e2f5bca1a
Zoltan Nagy <abesto@abesto.net>
2015-03-06 12:11:06 +0100

    Refactor, write basic tests

--- index.js
+++ index.js
@@ -1,67 +1,80 @@
 /* global require, console, exports */
-var layouts = require('log4js/lib/layouts.js'),
-    scribe = require('scribe'),
-    os = require('os'),
-    passThrough = layouts.messagePassThroughLayout;
-
-/**
- * Scribe Appender for log4js. Sends logging events via Scribe using node-scribe.
- *
- * @param config object with Scribe configuration data
- * @param layout function that takes a log event and returns a string
- */
-function scribeAppender(config, layout) {
-    'use strict';
-    config = config || {};
-    config.host = config.host || 'localhost';
-    config.port = config.port || 1463;
-
-    var scribeCategory = config.scribeCategory || 'log4js-node';
-
-    var client = new scribe.Scribe(config.host, config.port, {autoReconnect:true});
-    var isReady = false;
-    var STORED_MESSAGES_LIMIT = 1000; // number of messages we store until scribe is fully connected
-    var storedMessages = [];
-
-    client.open(function(err){
-
-        if(err) {
-            return console.log(err);
-        }
-        while (storedMessages.length) {
-            var msg = storedMessages.shift();
-            client.send(scribeCategory, msg);
-        }
-        isReady = true;
-    });
-
-    client.on('error', function(err){
-        console.log("Couldn't connect to scribe: ", err.code);
-    });
-
-    if(!layout) {
-        layout = passThrough;
-    }
-
-    return function(loggingEvent) {
-        var msg = layout(loggingEvent);
-        if (isReady) {
-            client.send(scribeCategory, msg);
-        } else if (storedMessages.length < STORED_MESSAGES_LIMIT) {
-            storedMessages.push(msg);
-        }
-    };
-}
-
-function configure(config) {
+(function() {
     "use strict";
-    var layout;
-    if (config.layout) {
-        layout = layouts.layout(config.layout.type, config.layout);
-    }
-    return scribeAppender(config, layout);
-}
+    
+    var layouts = require('log4js/lib/layouts.js'),
+        scribeModule = require('scribe'),
+        os = require('os'),
+        passThrough = layouts.messagePassThroughLayout;
 
-exports.name      = 'scribe';
-exports.appender  = scribeAppender;
-exports.configure = configure;
+    /**
+     * Scribe Appender for log4js. Sends logging events via Scribe using node-scribe.
+     *
+     * @param category the Scribe category to log to
+     * @param layout function that takes a log event and returns a string
+     * @param client Scribe client instance
+     */
+    function scribeAppender(category, layout, client) {
+        var isReady = false;
+        var STORED_MESSAGES_LIMIT = 1000; // number of messages we store until scribe is fully connected
+        var storedMessages = [];
+
+        client.open(function(err){
+
+            if(err) {
+                return console.log(err);
+            }
+            while (storedMessages.length) {
+                var msg = storedMessages.shift();
+                client.send(category, msg);
+            }
+            isReady = true;
+        });
+
+        client.on('error', function(err){
+            console.log("Couldn't connect to scribe: ", err.code);
+        });
+
+        if(!layout) {
+            layout = passThrough;
+        }
+
+        return function(loggingEvent) {
+            var msg = layout(loggingEvent);
+            if (isReady) {
+                client.send(category, msg);
+            } else if (storedMessages.length < STORED_MESSAGES_LIMIT) {
+                storedMessages.push(msg);
+            }
+        };
+    }
+
+    function withDefaults(_config) {
+        var config = _config || {};
+        config.host = config.host || 'localhost';
+        config.port = config.port || 1463;
+        config.scribeCategory = config.scribeCategory || 'log4js-node';
+        return config;
+    }
+
+    function makeLayout(config) {
+        if (config.layout) {
+            return layouts.layout(config.layout.type, config.layout);
+        }
+        return null;
+    }
+
+    function configure(_config, _scribe) {
+        var scribe = _scribe || scribeModule;
+        var config = withDefaults(_config);
+        var client = new scribe.Scribe(config.host, config.port, {autoReconnect:true});
+        var layout = makeLayout(config);
+        return scribeAppender(config.scribeCategory, layout, client);
+    }
+
+    exports.name      = 'scribe';
+    exports.appender  = scribeAppender;
+    exports.configure = configure;
+    exports.withDefaults = withDefaults;
+}());
+
---
+++ test.js
@@ -0,0 +1,27 @@
+/* global require, describe, it */
+(function () {
+    "use strict";
+    var assert = require("assert");
+    var sinon = require("sinon");
+    var scribe = require("scribe");
+    
+    var log4jScribeAppender = require("./index");
+
+    describe('withDefaults', function(){
+        it('sets correct defaults for config = null', function() {
+            var config = log4jScribeAppender.withDefaults(null);
+            assert.equal(config.host, 'localhost');
+            assert.equal(config.port, 1463);
+            assert.equal(config.scribeCategory, 'log4js-node');
+        });
+        
+        it('doesn\'t override passed in values', function() {
+            var _config = { host: 'foo', port: 42, scribeCategory: 'bar' };
+            var config = log4jScribeAppender.withDefaults(_config);
+            assert.equal(config.host, _config.host);
+            assert.equal(config.port, _config.port);
+            assert.equal(config.scribeCategory, _config.scribeCategory);
+        });
+    });
+}());
+
